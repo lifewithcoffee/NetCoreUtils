259d5cad20039ba4eccb5eeb6d5e833847ad3c1b
Ron <li.rongcheng@gmail.com>
2018-07-31 19:31:18 +1000

    add XmlUtil

---
+++ NetCoreUtils.XunitTest/XmlUtilTests.cs
@@ -0,0 +1,112 @@
+using NetCoreUtils.Xml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using Xunit;
+
+namespace NetCoreUtils.XunitTest
+{
+    public class MyXmlUtilObject
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        public int Length { get; set; }
+
+        [XmlIgnore]
+        public int Ignored { get; set; }
+
+        public List<ChildObject> ChildObjects { get; set; } = new List<ChildObject>();
+
+        [XmlElement("AnotherList")]
+        public List<ChildObject2> ChildObject2List { get; set; } = new List<ChildObject2>();
+    }
+
+    public class ChildObject
+    {
+        [XmlAttribute]
+        public string Prop { get; set; }
+
+        [XmlText]
+        public string Value { get; set; }
+    }
+
+    public class ChildObject2
+    {
+        [XmlAttribute]
+        public int Value { get; set; }
+    }
+
+    public class XmlUtilTests
+    {
+        MyXmlUtilObject obj = new MyXmlUtilObject
+        {
+            Name = "test name 123",
+            Length = 123,
+            Ignored = 56789,
+            ChildObjects = new List<ChildObject>
+            {
+                new ChildObject { Prop = "中文测试，某属性", Value = "value 1" },
+                new ChildObject { Prop = "prop abc", Value = "中文测试，某值" },
+                new ChildObject { Prop = "prop xyz 1234", Value = "prop xyz 1234" },
+            },
+            ChildObject2List = new List<ChildObject2>
+            {
+                new ChildObject2 { Value = 11 },
+                new ChildObject2 { Value = 22 },
+                new ChildObject2 { Value = 333 },
+                new ChildObject2 { Value = 4444 },
+            }
+        };
+
+        [Fact]
+        public void To_from_file()
+        {
+            new XmlUtil<MyXmlUtilObject>().WriteToFile(obj, @"d:\xml_test_output_file.xml");
+
+            var de = new XmlUtil<MyXmlUtilObject>().ReadFromFile(@"d:\xml_test_output_file.xml");
+            Assert.Equal(de.Name, obj.Name);
+            Assert.Equal(de.Length, obj.Length);
+
+            // because "Ignored" is an ignored element, which won't be serialized to the file;
+            // thus, when deserialize from the file it won't be signed the number it was
+            Assert.NotEqual(de.Ignored, obj.Ignored);
+            Assert.Equal(0, de.Ignored);    
+
+            Assert.Equal(3, de.ChildObjects.Count);
+            Assert.Equal(de.ChildObjects[0].Prop, obj.ChildObjects[0].Prop);
+
+            Assert.Equal(4, de.ChildObject2List.Count);
+            Assert.Equal(de.ChildObject2List[0].Value, obj.ChildObject2List[0].Value);
+        } 
+
+        [Fact]
+        public void To_from_string()
+        {
+            string xmlstr = "";
+
+            // write to file: only for manual check output
+            using (StreamWriter file = new StreamWriter(@"d:\xml_text_output_string.xml"))
+            {
+                xmlstr = new XmlUtil<MyXmlUtilObject>(8).ConvertToString(obj);
+                file.Write(xmlstr); 
+            }
+
+            var de = new XmlUtil<MyXmlUtilObject>().ConvertFromString(xmlstr);
+            Assert.Equal(de.Name, obj.Name);
+            Assert.Equal(de.Length, obj.Length);
+
+            // because "Ignored" is an ignored element, which won't be serialized to the file;
+            // thus, when deserialize from the file it won't be signed the number it was
+            Assert.NotEqual(de.Ignored, obj.Ignored);
+            Assert.Equal(0, de.Ignored);
+
+            Assert.Equal(3, de.ChildObjects.Count);
+            Assert.Equal(de.ChildObjects[0].Prop, obj.ChildObjects[0].Prop);
+
+            Assert.Equal(4, de.ChildObject2List.Count);
+            Assert.Equal(de.ChildObject2List[0].Value, obj.ChildObject2List[0].Value);
+        }
+    }
+}
---
+++ NetCoreUtils/Xml/XmlUtil.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NetCoreUtils.Xml
+{
+    public class XmlUtil<T>
+    {
+        XmlWriterSettings settings = new XmlWriterSettings
+        {
+            Indent = true,
+            IndentChars = "    ",  // 4 spaces for each indention
+            OmitXmlDeclaration = true,
+            Encoding = Encoding.UTF8
+        };
+
+        public XmlUtil(int indentSpaceNumber = 4)
+        {
+            if (indentSpaceNumber > 0)
+            {
+                settings.Indent = true; // must be true if need to apply IndentChars
+                settings.IndentChars = new string(' ', indentSpaceNumber);
+            }
+            else
+                settings.Indent = false;
+        }
+
+        public void WriteToFile(T obj, string filepath)
+        {
+
+            Directory.CreateDirectory(Path.GetDirectoryName(filepath));
+            using (Stream file = new FileStream(filepath, FileMode.Create))
+            using (var writer = XmlWriter.Create(file, settings))
+            {
+                new XmlSerializer(typeof(T)).Serialize(writer, obj, new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }));
+            }
+        }
+
+        public T ReadFromFile(string filepath)
+        {
+            T retval = default(T);
+
+            if (File.Exists(filepath))
+            {
+                using (StreamReader file = new StreamReader(filepath))
+                {
+                    XmlSerializer ser = new XmlSerializer(typeof(T));
+                    retval = (T)ser.Deserialize(file);
+                }
+            }
+
+            return retval;
+        }
+
+        public string ConvertToString(T obj)
+        {
+            using (StringWriter sw = new StringWriter())
+            using (var writer = XmlWriter.Create(sw, settings))
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                ser.Serialize(writer, obj, new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }));
+                return sw.ToString();
+            }
+        }
+
+        public T ConvertFromString(string xmlString)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(xmlString);
+            using (MemoryStream mem = new MemoryStream(bytes))
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                return (T)ser.Deserialize(mem);
+            }
+        }
+    }
+}
