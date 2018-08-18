9f8e1d5cd7dfab275ed9d61a200a7b551d9d24de
Ron <li.rongcheng@gmail.com>
2018-08-18 21:53:26 +1000

    release NetCoreUtils v0.1.0.2: add ShortGuid

---
+++ NetCoreUtils.XunitTest/ShortGuidTests.cs
@@ -0,0 +1,27 @@
+﻿using NetCoreUtils.String;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NetCoreUtils.XunitTest
+{
+    public class ShortGuidTests
+    {
+        private readonly ITestOutputHelper output;
+        public ShortGuidTests(ITestOutputHelper output) { this.output = output; }
+
+        [Fact]
+        public void DoTest()
+        {
+            string shortGuid = ShortGuid.NewGuid();
+            output.WriteLine($"shortGuid\t: {shortGuid}");
+
+            string decodedGuid = ShortGuid.Decode(shortGuid).ToString();
+            output.WriteLine($"decodedGuid\t: {decodedGuid}");
+
+            Assert.Equal(shortGuid, ShortGuid.Encode(decodedGuid));
+        }
+    }
+}
---
+++ NetCoreUtils/FileOperation/FileSearcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NetCoreUtils.FileOperation
+{
+    public class FileSearcher
+    {
+        public IEnumerable<string> Find(string baseDir, string extensionName, params string[] keywords)
+        {
+            var matched = new DirectoryInfo(baseDir).EnumerateFiles($"*.{extensionName}", SearchOption.AllDirectories)
+                       .AsParallel()
+                       .Where(f => keywords.All(k => f.Name.ToLowerInvariant().Contains(k.ToLowerInvariant())));
+
+            return matched.Select(f => f.FullName);
+        }
+
+        public string CreateIndex(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
