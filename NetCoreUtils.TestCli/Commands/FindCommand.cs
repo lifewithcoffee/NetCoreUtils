70825abe11f1de3f26b928391e9ffa6ab3f6379c
Ron <li.rongcheng@gmail.com>
2020-05-19 21:03:42 +1000

    update mcn path

--- NetCoreUtils.TestCli/Commands/FindCommand.cs
+++ NetCoreUtils.TestCli/Commands/FindCommand.cs
@@ -12,7 +12,7 @@ namespace NetCoreUtils.TestCli.Commands
         /// <param name="keywords">Multiple keywords separated by ',' or ';'</param>
         public void File(string keywords)
         {
-            var files = _svc.Find(@"e:\rp\hg\mcn\sync\", "mcn", keywords.Split(',',';'));
+            var files = _svc.Find(@"e:\rp\mcn\sync\", "mcn", keywords.Split(',',';'));
             foreach(var file in files)
             {
                 Console.WriteLine(file);
