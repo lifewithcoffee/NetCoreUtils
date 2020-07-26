93dfac4435c0526f8dc43c14e4ab7d2701a373c6
Ron <li.rongcheng@gmail.com>
2020-07-27 01:32:15 +1000

    add reflection util command

---
+++ NetCoreUtils.TestCli/Commands/ReflectCommand.cs
@@ -0,0 +1,26 @@
+﻿using NetCoreUtils.TestCli.ReflectUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreUtils.TestCli.Commands
+{
+    public class ReflectCommand
+    {
+        public void CallImpls2()
+        {
+            Console.WriteLine("Foo2() called");
+            var list = new ReflectUtil().GetAllImplementationInstances2(typeof(ISomeInterface));
+            foreach (var obj in list)
+            {
+                ((ISomeInterface)obj).DoSomething();
+            }
+        }
+
+        public void CallImpls1()
+        {
+            Console.WriteLine("Foo1() called");
+            new ReflectUtil().GetAllImplementationInstances1<ISomeInterface>().ForEach(x => x.DoSomething());
+        }
+    }
+}
---
+++ NetCoreUtils.TestCli/ReflectUtils/ReflectUtil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetCoreUtils.TestCli.ReflectUtils
+{
+    public interface ISomeInterface
+    {
+        void DoSomething();
+    }
+
+    public class Impl1 : ISomeInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Do1.DoSomething() called");
+        }
+    }
+
+    public class Impl2 : ISomeInterface
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Do2.DoSomething() called");
+        }
+    }
+
+    /// <summary>
+    /// Methods in this class can be used to implemnt an DI auto-injector
+    /// </summary>
+    public class ReflectUtil
+    {
+
+        public IEnumerable<object> GetAllImplementationInstances2(Type interfaceType)
+        {
+            var interfaceImpls = typeof(Program)
+                .Assembly
+                .ExportedTypes
+                .Where(x => interfaceType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)      // make sure the type is an initializable class that implements the interface
+                .Select(Activator.CreateInstance);
+
+            return interfaceImpls;
+        }
+
+        public List<T> GetAllImplementationInstances1<T>()
+        {
+            var interfaceImpls = typeof(Program)
+                .Assembly
+                .ExportedTypes
+                .Where(x => typeof(T).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)      // make sure the type is an initializable class that implements the interface
+                .Select(Activator.CreateInstance).Cast<T>().ToList();
+
+            return interfaceImpls;
+        }
+    }
+}
