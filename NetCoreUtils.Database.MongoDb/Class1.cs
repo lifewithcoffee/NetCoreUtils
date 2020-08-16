aa0ac325c364781d57c954829dcc5cdc32d4e007
Ron <li.rongcheng@gmail.com>
2020-08-17 03:37:31 +1000

    implementing AddMongoDb() method

--- NetCoreUtils.Database.MongoDb/Class1.cs
+++ NetCoreUtils.Database.MongoDb/Class1.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 using System;
 
 namespace NetCoreUtils.Database.MongoDb
 {
+    public class MongoDbSetting
+    {
+        public string Host { get; set; } = "localhost";
+        public int Port { get; set; } = 27017;
+    }
+
     static public class Class1
     {
-        static public void AddMongoDb(this IServiceCollection services)
+        static public void AddMongoDb(this IServiceCollection services, MongoDbSetting setting)
         {
-
+            services.AddSingleton<MongoClient>(x => new MongoClient($"mongodb://{setting.Host}:{setting.Port}"));
         }
     }
 }
