e819dd730019f8d659303875a7651b8f13e6f372
Ron <li.rongcheng@gmail.com>
2020-08-25 21:52:26 +1000

    update test cli: add mong3 command

--- NetCoreUtils.TestCli/Commands/MongodbCommand.cs
+++ NetCoreUtils.TestCli/Commands/MongodbCommand.cs
@@ -15,18 +15,12 @@ namespace NetCoreUtils.TestCli.Commands
 
     public class Mongo2Command
     {
-        IMongoDbConnection mongo;
 
         // note: the following members use different collection names, see the constructor for more details
         IMongoCollection<TestDoc> collection;
-        IMongoDocReader<Student> reader;
 
-        public Mongo2Command(IMongoDbConnection connection, IMongoDocReader<Student> reader)
+        public Mongo2Command(IMongoDbConnection connection)
         {
-            this.reader = reader;
-            this.reader.CollectionName = "rl_test_colle2";
-
-            this.mongo = connection;
             this.collection = connection.MongoDatabase.GetCollection<TestDoc>("TestDocs");
         }
 
@@ -47,8 +41,35 @@ namespace NetCoreUtils.TestCli.Commands
             var doc = collection.Find("5f3bbd65f63d7d5175a805d8");
             Console.WriteLine(doc.ToJson());
         }
+    }
 
-        public void ReadAllDocs()
+    public class Mongo3Command
+    {
+
+        IMongoDocReader<Student> reader;
+        IMongoDocWriter<Student> writer;
+
+        public Mongo3Command(IMongoDocReader<Student> reader, IMongoDocWriter<Student> writer)
+        {
+            this.reader = reader;
+            this.writer = writer;
+            Console.WriteLine($"readers collection name = {reader.CollectionName}");
+        }
+
+        public async Task Add()
+        {
+            await writer.InsertOneAsync(new Student
+            {
+                student_id = 3432,
+                class_id = 12,
+                scores = new List<Score> {
+                    new Score { type = "English", score = 89 },
+                    new Score{ type = "Math", score = 91 },
+                }
+            });
+        }
+
+        public void ReadAll()
         {
             var docs = reader.Find(d => true);
             foreach (var doc in docs)
@@ -64,6 +85,7 @@ namespace NetCoreUtils.TestCli.Commands
         public double score;
     }
 
+    //[Collection("my_test_collection_name")]
     public class Student : MongoDoc
     {
         public int student_id;
