5132a816af1572d4cd2d9518e5df959875550bad
Ron <li.rongcheng@gmail.com>
2020-10-30 01:06:30 +1100

    update test commands

--- NetCoreUtils.TestCli/Commands/Influx/InfluxDiCommand.cs
+++ NetCoreUtils.TestCli/Commands/Influx/InfluxDiCommand.cs
@@ -1,10 +1,13 @@
 ﻿using CoreCmd.Attributes;
 using InfluxDB.Client.Api.Domain;
 using InfluxDB.Client.Writes;
+using MongoDB.Bson;
 using NetCoreUtils.Database.InfluxDb;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NetCoreUtils.TestCli.Commands.Influx
@@ -21,14 +24,14 @@ namespace NetCoreUtils.TestCli.Commands.Influx
             _reader = reader;
         }
 
-        public async Task Write()
+        public async Task WritePoco()
         {
             var random = new Random();
             //await _writer.WriteAsync(new Temperature { Location = "UNSW", Value = 94D, RLTestColumn = 123D, Time = DateTime.UtcNow });
             await _writer.WriteAsync(new Temperature { Location = "UNSW", Value = random.Next(0,100), RLTestColumn = random.Next(0,100), Time = DateTime.UtcNow });
         }
 
-        public async Task WriteList()
+        public async Task WritePocoList()
         {
             List<Temperature> list = new List<Temperature>();
             var random = new Random();
@@ -48,7 +51,7 @@ namespace NetCoreUtils.TestCli.Commands.Influx
             Console.WriteLine("WriteList() called");
         }
 
-        public async Task WriteArray(int number)
+        public async Task WritePocoArray(int number)
         {
             Temperature[] array = new Temperature[number];
             var random = new Random();
@@ -67,17 +70,130 @@ namespace NetCoreUtils.TestCli.Commands.Influx
             await _writer.WriteAsync(array);
         }
 
-        public async Task WritePoint() 
+        public async Task WritePoint()
         {
             var value = new Random().Next(0, 100);
-            var point = PointData.Measurement("my-point")
+
+            var point = PointData.Measurement("my-point2")
                 .Tag("tag1", "PointTag1")
                 .Tag("tag2", "PointTag2")
                 .Field("value", value)
                 .Timestamp(DateTime.UtcNow, WritePrecision.Ns);
 
+            /** Or:
+             * var point = PointData.Measurement("my-point2");
+             * point = point.Tag("tag1", "PointTag1");  // just doing 'pointData.Tag("tag1", "PointTag1");' doesn't work, this might be a bug 
+             * point = point.Tag("tag2", "PointTag2");
+             * point = point.Field("value", value);
+             * point = point.Timestamp(DateTime.UtcNow, WritePrecision.Ns); 
+             **/
+
             await _writer.WriteAsync(point);
             Console.WriteLine($"WritePoint():  {value}");
+        }
+
+        public async Task WritePointModel(string measurementName)
+        { 
+            var value = new Random().Next(0, 100);
+
+            var point2 = new PointModel<long>();
+            point2.Measurement = measurementName;
+            point2.Tags["tag3"] = "test-tag31";
+            point2.Tags["tag4"] = "test-tag41";
+            point2.Fields["value1"] = value;
+            point2.Fields["value2"] = value + 2;
+            
+            // try to (de)serialize to/from json
+            Console.WriteLine($"WritePoint(), use System.Text.Json:\n{JsonSerializer.Serialize(point2, new JsonSerializerOptions { WriteIndented = true })}");
+            Console.WriteLine($"WritePoint(), use mongodb's ToJson:\n{point2.ToJson()}");
+
+            await _writer.WriteAsync(point2);
+        }
+
+        public async Task WritePointModelList(string measurementName, int number)
+        {
+            var random = new Random();
+
+            List<PointModel<long>> list = new List<PointModel<long>>();
+
+            for(int i = 0; i < number; i++)
+            {
+                var value = random.Next(0, 100);
+                var point =
+                    new PointModel<long>
+                    {
+                        Measurement = measurementName,
+                        Tags = { { "tag3", "test-tag31" }, { "tag4", "test-tag41" } },
+                        Fields = { { "value1", value }, { "value2", value + 2 } },
+                        Timestamp = DateTime.UtcNow.AddSeconds(-i)
+                    };
+
+                list.Add(point);
+                Console.WriteLine(point.ConverToPointData().ToLineProtocol());
+            }
+
+            await _writer.WriteAsync(list);
+        }
+
+        public async Task WritePointModelArray(string measurementName, int number)
+        {
+            var random = new Random();
+
+            PointModel<long>[] array = new PointModel<long>[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                var value = random.Next(0, 100);
+                var point =
+                    new PointModel<long>
+                    {
+                        Measurement = measurementName,
+                        Tags = { { "tag3", "test-tag31" }, { "tag4", "test-tag41" } },
+                        Fields = { { "value1", value }, { "value2", value + 2 } },
+                        Timestamp = DateTime.UtcNow.AddSeconds(-i)
+                    };
+
+                array[i] = point;
+                Console.WriteLine(point.ConverToPointData().ToLineProtocol());
+            }
+
+            await _writer.WriteAsync(array);
+        }
+
+        public async Task WriteDeserializedPoint(string measurementName)
+        {
+            try
+            {
+                string dateTimeString = JsonSerializer.Serialize(DateTime.UtcNow);
+                Console.WriteLine($"Calling WriteDeserializedPoint()...{dateTimeString}");
+
+                string json =
+                    "{\"Measurement\": \""
+                    + measurementName
+                    + "\", \"Tags\": { \"tag3\": \"test-tag31\", \"tag4\": \"test-tag41\" }, \"Fields\": { \"value1\": 28, \"value2\": 30 }, \"Timestamp\": "
+                    + dateTimeString
+                    + "}";
+                //+ "2020-10-29T01:32:31.8118638Z\" }"
+                Console.WriteLine($"json string:\n{json}");
+
+                var point3 = JsonSerializer.Deserialize<PointModel<long>>(json);
+
+                await _writer.WriteAsync(point3);
+
+                var point4 = point3.ConverToPointData();
+
+                Debug.Assert(point3.Measurement == measurementName);
+                Debug.Assert(point3.Tags["tag3"] == "test-tag31");
+                Debug.Assert(point3.Tags["tag4"] == "test-tag41");
+                Debug.Assert(point3.Fields["value1"] == 28);
+                Debug.Assert(point3.Fields["value2"] == 30);
+
+                Console.WriteLine("All correct");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public async Task WritePointList()
