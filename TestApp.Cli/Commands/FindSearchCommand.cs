158ba097284761a02f150391602d10a8d1f6a564
Ron <li.rongcheng@gmail.com>
2021-03-08 15:15:32 +1100

    change to multi-keyword file content search

---
+++ TestApp.Cli/Commands/FindSearchCommand.cs
@@ -0,0 +1,51 @@
+﻿using CoreCmd.Attributes;
+using NetCoreUtils.FileOperation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.Cli.Commands
+{
+    [Help("Search file")]
+    [Alias("fs")]
+    class FileSearchCommand
+    {
+        FileSearcher _svc = new FileSearcher();
+
+        /// <param name="keywords">Multiple keywords separated by ',' or ';'</param>
+        public void Name(string keywords)
+        {
+            var files = _svc.Find(@"i:\rp\mcn\sync\", "mcn", keywords.Split(',',';'));
+            foreach(var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public void Content(string k)
+        {
+            string baseDir = @"i:\rp\mcn\sync\";
+            string extensionName = "mcn";
+            string[] keywords = k.Split(',', ';');
+
+            var filesWithAllKeywords = new DirectoryInfo(baseDir).EnumerateFiles($"*.{extensionName}", SearchOption.AllDirectories)
+                                  .AsParallel()     // RL: it does increase the performance
+                                  .Select(f => (f.FullName, File.ReadAllText(f.FullName)))
+                                  .Where(t => keywords.All(k => t.Item2.ToLowerInvariant().Contains(k.ToLowerInvariant())));
+
+
+            IEnumerable<string> linesWithAnyKeywords = filesWithAllKeywords
+                                                       .SelectMany(f => File.ReadLines(f.FullName).Select(line => (f.FullName, line))
+                                                       .Where(t => keywords.Any(k => t.line.ToLowerInvariant().Contains(k.ToLowerInvariant()))))
+                                                       .OrderBy(t=> t.FullName)
+                                                       .Select(t => $"{t.FullName}|{t.line.Trim()}");
+
+            foreach(var line in linesWithAnyKeywords)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
