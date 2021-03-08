9586784a3388ccb2867166aef04b560846d1d0f4
Ron <li.rongcheng@gmail.com>
2021-03-09 00:59:50 +1100

    Update FindSearchCommand.cs

--- TestApp.Cli/Commands/FindSearchCommand.cs
+++ TestApp.Cli/Commands/FindSearchCommand.cs
@@ -2,12 +2,23 @@
 using NetCoreUtils.FileOperation;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace TestApp.Cli.Commands
 {
+    class FileLineDict
+    {
+        Dictionary<string, List<string>> fileLineDicts = new Dictionary<string, List<string>>();
+
+        public void AddLine(string fileFullPath, string line)
+        {
+            fileLineDicts[fileFullPath].Add(line);
+        }
+    }
+
     [Help("Search file")]
     [Alias("fs")]
     class FileSearchCommand
@@ -24,27 +35,83 @@ namespace TestApp.Cli.Commands
             }
         }
 
-        public void Content(string k)
+        public void Content(string keywords)
         {
             string baseDir = @"i:\rp\mcn\sync\";
             string extensionName = "mcn";
-            string[] keywords = k.Split(',', ';');
+            string[] keywordArray = keywords.Split(',', ';');
 
             var filesWithAllKeywords = new DirectoryInfo(baseDir).EnumerateFiles($"*.{extensionName}", SearchOption.AllDirectories)
                                   .AsParallel()     // RL: it does increase the performance
                                   .Select(f => (f.FullName, File.ReadAllText(f.FullName)))
-                                  .Where(t => keywords.All(k => t.Item2.ToLowerInvariant().Contains(k.ToLowerInvariant())));
+                                  .Where(t => keywordArray.All(k => t.Item2.ToLowerInvariant().Contains(k.ToLowerInvariant())));
 
 
-            IEnumerable<string> linesWithAnyKeywords = filesWithAllKeywords
-                                                       .SelectMany(f => File.ReadLines(f.FullName).Select(line => (f.FullName, line))
-                                                       .Where(t => keywords.Any(k => t.line.ToLowerInvariant().Contains(k.ToLowerInvariant()))))
-                                                       .OrderBy(t=> t.FullName)
-                                                       .Select(t => $"{t.FullName}|{t.line.Trim()}");
+            Dictionary<string, List<string>> fileLineDicts = new Dictionary<string, List<string>>();
 
-            foreach(var line in linesWithAnyKeywords)
+            // init dict: generate list instance for all keys
+            foreach (var filePath in filesWithAllKeywords)
             {
-                Console.WriteLine(line);
+                fileLineDicts[filePath.FullName] = new List<string>();
+            }
+
+
+
+
+            //IEnumerable<string> linesWithAnyKeywords = filesWithAllKeywords
+            var linesWithAnyKeywords = filesWithAllKeywords
+                                                       .SelectMany(f => File.ReadLines(f.FullName).Select(line => (f.FullName, line))
+                                                       .Where(t => keywordArray.Any(k => t.line.ToLowerInvariant().Contains(k.ToLowerInvariant()))))
+                                                       .GroupBy(t => t.FullName);
+
+            int count = 1;
+            int foundFileNumber = linesWithAnyKeywords.Count();
+            string[] files = new string[foundFileNumber];
+
+            foreach (var lines in linesWithAnyKeywords)
+            {
+                string fileFullName = lines.Key.Trim();
+                files[count - 1] = fileFullName;
+
+                Console.WriteLine($"{count}|{fileFullName}");
+                Console.WriteLine($"{count}|{"".PadLeft(fileFullName.Length, '-')}");
+
+                var lineEnumerator = lines.ToList();
+                foreach (var line in lineEnumerator)
+                {
+                    Console.WriteLine($"{count}|{line.line.Trim()}");
+                }
+                count++;
+                Console.WriteLine();
+            }
+
+            Console.Write("Please input the selection number to open a file (input 'q' to quit):");
+            string readline = Console.ReadLine();
+
+            while (!readline.Equals("q"))
+            {
+                try
+                {
+                    int select = Convert.ToInt32(readline);
+
+                    if (select < 1 || select > foundFileNumber)
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+                    else
+                    {
+                        Process.Start(new ProcessStartInfo(files[select - 1])
+                        {
+                            UseShellExecute = true  // UseShellExecute is false by default on .NET Core.
+                        });
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+                Console.Write("Please input the selection number to open a file (input 'q' to quit):");
+                readline = Console.ReadLine();
             }
         }
     }
