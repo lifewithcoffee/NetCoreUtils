e12f355d9825a750229bb6d6fe11fcccfb9e9738
li-rongcheng <Li.Rongcheng@gmail.com>
2021-05-19 19:40:11 +1000

    rcm: redesign commands

--- Applications/RobocopyConfigManager/Commands/BackupCommand.cs
+++ Applications/RobocopyConfigManager/Commands/BackupCommand.cs
@@ -1,69 +1,64 @@
-﻿using NetCoreUtils.Text.Json;
+﻿using NetCoreUtils.ProcessUtils;
+using NetCoreUtils.Text.Json;
 using RobocopyConfigManager.Misc;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace RobocopyConfigManager.Commands
+namespace TestApp.Cli.Commands
 {
     class BackupCommand
     {
-        /// <summary>
-        /// TODO: backup update
-        /// - group name shall be unique
-        /// - names in a group shall be unique
-        /// - update-group shall work even if the group doesn't exist
-        ///
-        /// Done:
-        /// - multiple sources shouldn't point to the same target
-        /// </summary>
-        /// <param name="fullBackupItemName">Backup item name with group prefix, e.g. SomeGroupName.SomeBackupName</param>
-        /// <param name="source">Backup source</param>
-        /// <param name="target">Backup target</param>
-        public void Update(string fullBackupItemName, string source, string target)
+        // TODO: backup the default group
+        public void Default()
         {
-            try
+            Console.WriteLine("backup default");
+        }
+
+        // TODO: backup item
+        public void Item(string fullBackupItemName)
+        {
+            Console.WriteLine($"backup item: {fullBackupItemName}");
+        }
+
+        public void Group(string groupName)
+        {
+            const string robocopy = "robocopy";
+            if (!ProcUtil.Exists(robocopy))
             {
-                var config = JsonConfigOperator<RobocopyConfig>.LoadCreate(RobocopyConfigParameters.fullConfigFilePath);
+                Console.Error.WriteLine($"Cannot find {robocopy}, program terminated");
+                return;
+            }
 
-                string trimmedGroupName = fullBackupItemName.Trim();
-                var names = trimmedGroupName.Split('.');
-                if (names.Length > 2)
-                {
-                    throw new Exception($"Invalid full backup item name: {fullBackupItemName}");
-                }
-                string groupName = names[0];
-                string backupItemName = names[1];
+            var config = JsonConfigOperator<RobocopyConfig>.LoadCreate(RobocopyConfigParameters.fullConfigFilePath);
 
-                // find the group with the specified name
-                foreach (var group in config.BackupGroups)
+            foreach (var group in config.BackupGroups)
+            {
+                if(group.GroupName == groupName)
                 {
-                    if (group.GroupName == groupName)
+                    string flags = $"/MT:16 /R:1 /W:3 /MIR /FFT /NP /LOG+:recover_{groupName}-{new Random().Next(0,99999)}.log";
+
+                    bool firstItem = true;
+                    foreach(var backup in group.Backups)
                     {
-                        bool foundTargetItem = false;
-
-                        // find the item with the same target
-                        foreach (var item in group.Backups)
+                        if(!firstItem)
                         {
-                            if (item.Target == target)
-                            {
-                                item.BackupName = backupItemName;
-                                item.Source = source;
-                                foundTargetItem = true;
-                            }
+                            Console.WriteLine("-----------------------------------------------");
                         }
-
-                        if (!foundTargetItem)
-                        {
-                            group.Backups.Add(new BackupItem { BackupName = backupItemName, Source = source, Target = target });
-                        }
+                        string arguments = $"\"{backup.Source}\" \"{backup.Target}\" {flags}";
+                        Console.WriteLine($"Executing: {robocopy} {arguments}");
+                        ProcUtil.Run(robocopy, arguments);
+                        firstItem = false;
                     }
                 }
-
-                JsonConfigOperator<RobocopyConfig>.Save(RobocopyConfigParameters.fullConfigFilePath, config);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
             }
         }
+
+        //public void Test()
+        //{
+        //    Console.WriteLine("test");
+        //}
     }
 }
---
+++ Applications/RobocopyConfigManager/Commands/ItemCommand.cs
@@ -0,0 +1,69 @@
+﻿using NetCoreUtils.Text.Json;
+using RobocopyConfigManager.Misc;
+using System;
+
+namespace RobocopyConfigManager.Commands
+{
+    class ItemCommand
+    {
+        /// <summary>
+        /// TODO: backup update
+        /// - group name shall be unique
+        /// - names in a group shall be unique
+        /// - update-group shall work even if the group doesn't exist
+        ///
+        /// Done:
+        /// - multiple sources shouldn't point to the same target
+        /// </summary>
+        /// <param name="fullBackupItemName">Backup item name with group prefix, e.g. SomeGroupName.SomeBackupName</param>
+        /// <param name="source">Backup source</param>
+        /// <param name="target">Backup target</param>
+        public void Update(string fullBackupItemName, string source, string target)
+        {
+            try
+            {
+                var config = JsonConfigOperator<RobocopyConfig>.LoadCreate(RobocopyConfigParameters.fullConfigFilePath);
+
+                string trimmedGroupName = fullBackupItemName.Trim();
+                var names = trimmedGroupName.Split('.');
+                if (names.Length > 2)
+                {
+                    throw new Exception($"Invalid full backup item name: {fullBackupItemName}");
+                }
+                string groupName = names[0];
+                string backupItemName = names[1];
+
+                // find the group with the specified name
+                foreach (var group in config.BackupGroups)
+                {
+                    if (group.GroupName == groupName)
+                    {
+                        bool foundTargetItem = false;
+
+                        // find the item with the same target
+                        foreach (var item in group.Backups)
+                        {
+                            if (item.Target == target)
+                            {
+                                item.BackupName = backupItemName;
+                                item.Source = source;
+                                foundTargetItem = true;
+                            }
+                        }
+
+                        if (!foundTargetItem)
+                        {
+                            group.Backups.Add(new BackupItem { BackupName = backupItemName, Source = source, Target = target });
+                        }
+                    }
+                }
+
+                JsonConfigOperator<RobocopyConfig>.Save(RobocopyConfigParameters.fullConfigFilePath, config);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
