9a8db37f7520e57f577b1c0ca0a2390a730c7b20
li-rongcheng <Li.Rongcheng@gmail.com>
2021-05-21 23:19:01 +1000

    rcm: merge item commands to group and rename group to default

--- Applications/RobocopyConfigManager/Commands/DefaultCommand.cs
+++ Applications/RobocopyConfigManager/Commands/DefaultCommand.cs
@@ -1,13 +1,110 @@
-﻿using NetCoreUtils.Text.Json;
+﻿using CoreCmd.Attributes;
+using NetCoreUtils.MethodCall;
+using NetCoreUtils.ProcessUtils;
+using NetCoreUtils.Text.Json;
 using RobocopyConfigManager.Misc;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace RobocopyConfigManager.Commands
 {
-    class ItemCommand
+    class DefaultCommand
     {
         RobocopyConfig config = JsonConfigOperator<RobocopyConfig>.LoadCreate(RobocopyConfigParameters.fullConfigFilePath);
+
+        [Help("Add a new backup group")]
+        public void Add(string groupName)
+        {
+            SafeCall.Execute(() => {
+                config.BackupGroups.Add(new BackupItemGroup { GroupName = groupName.Trim() });
+                JsonConfigOperator<RobocopyConfig>.Save(RobocopyConfigParameters.fullConfigFilePath, config);
+            });
+        }
+
+        [Help("List available backup groups")]
+        public void List()
+        {
+            SafeCall.Execute(() => {
+                foreach(var group in config.BackupGroups)
+                {
+                    Console.WriteLine(group.GroupName);
+                }
+            });
+        }
+
+        [Help("List backup items in the specified backup group")]
+        public void List(string groupName)
+        {
+            SafeCall.Execute(() => {
+                var group = config.GetGroup(groupName);
+                if(group != null)
+                {
+                    foreach(var item in group.BackupItems)
+                    {
+                        Console.WriteLine($"Name: {item.BackupName}");
+                        Console.WriteLine($"Source: {item.Source}");
+                        Console.WriteLine($"Backup: {item.Target}");
+                        Console.WriteLine("");
+                    }
+                }
+            });
+        }
+
+        [Help("Remove a backup group")]
+        public void Remove(string groupName)
+        {
+            SafeCall.Execute(() => {
+                config.RemoveGroup(groupName);
+                JsonConfigOperator<RobocopyConfig>.Save(RobocopyConfigParameters.fullConfigFilePath, config);
+            });
+        }
+
+        [Help("Rename a backup group")]
+        public void Rename(string targetGroupName, string newName)
+        {
+            SafeCall.Execute(() => {
+                config.RenameGroup(targetGroupName, newName);
+                JsonConfigOperator<RobocopyConfig>.Save(RobocopyConfigParameters.fullConfigFilePath, config);
+            });
+        }
+
+        [Help("Sychronize from the source directory to the target directory")]
+        public void Backup(string groupName)
+        {
+            const string robocopy = "robocopy";
+            if (!ProcUtil.Exists(robocopy))
+            {
+                Console.Error.WriteLine($"Cannot find {robocopy}, program terminated");
+                return;
+            }
+
+            var config = JsonConfigOperator<RobocopyConfig>.LoadCreate(RobocopyConfigParameters.fullConfigFilePath);
+
+            foreach (var group in config.BackupGroups)
+            {
+                if (group.GroupName == groupName)
+                {
+                    string flags = $"/MT:16 /R:1 /W:3 /MIR /FFT /NP /LOG+:recover_{groupName}-{new Random().Next(0, 99999)}.log";
+
+                    bool firstItem = true;
+                    foreach (var backup in group.BackupItems)
+                    {
+                        if (!firstItem)
+                        {
+                            Console.WriteLine("-----------------------------------------------");
+                        }
+                        string arguments = $"\"{backup.Source}\" \"{backup.Target}\" {flags}";
+                        Console.WriteLine($"Executing: {robocopy} {arguments}");
+                        ProcUtil.Run(robocopy, arguments);
+                        firstItem = false;
+                    }
+                }
+            }
+        }
 
         private bool AreValidSourceAndTarget(string source, string target)
         {
@@ -26,7 +123,7 @@ namespace RobocopyConfigManager.Commands
             string normalizedSource = new DirectoryInfo(source).FullName.Trim('\\');
             string normalizedTarget = new DirectoryInfo(target).FullName.Trim('\\');
 
-            if(normalizedSource == normalizedTarget)
+            if (normalizedSource == normalizedTarget)
             {
                 Console.WriteLine($"The source and target pathes should be different.");
                 return false;
@@ -52,10 +149,10 @@ namespace RobocopyConfigManager.Commands
             {
                 // validate to make sure there is not any existing backup configured with the same target
                 string lowerTarget = target.ToLower();
-                foreach(var backupItem in group.BackupItems)
+                foreach (var backupItem in group.BackupItems)
                 {
                     // skip over the specified item, only apply validation to other items
-                    if(backupItem.BackupName != itemName)
+                    if (backupItem.BackupName != itemName)
                     {
                         if (backupItem.Target.ToLower() == lowerTarget)
                         {
@@ -67,7 +164,7 @@ namespace RobocopyConfigManager.Commands
 
                 // do update
                 var item = group.GetItem(itemName);
-                if(item != null)
+                if (item != null)
                 {
                     item.Source = source;
                     item.Target = target;
@@ -90,7 +187,7 @@ namespace RobocopyConfigManager.Commands
         {
             var group = config.GetGroup(groupName);
 
-            if(group != null)
+            if (group != null)
             {
                 group.BackupItems.Remove(group.GetItem(itemName));
             }
@@ -106,10 +203,10 @@ namespace RobocopyConfigManager.Commands
         {
             var group = config.GetGroup(groupName);
 
-            if(group != null)
+            if (group != null)
             {
                 // check if an backup item with the same new name has existed
-                foreach(var backupItem in group.BackupItems)
+                foreach (var backupItem in group.BackupItems)
                 {
                     if (backupItem.BackupName == newItemName)
                     {
@@ -120,7 +217,7 @@ namespace RobocopyConfigManager.Commands
 
                 // do renaming
                 var item = group.GetItem(currentItemName);
-                if(item != null)
+                if (item != null)
                 {
                     item.BackupName = newItemName;
                 }
