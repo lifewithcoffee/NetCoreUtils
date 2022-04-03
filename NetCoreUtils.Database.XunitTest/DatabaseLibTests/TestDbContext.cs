6630ad1deab0a7584905e49a411190fb5bde2e9e
li-rongcheng <Li.Rongcheng@gmail.com>
2022-04-04 01:47:17 +1000

    update unit test

--- NetCoreUtils.Database.XunitTest/DatabaseLibTests/TestDbContext.cs
+++ NetCoreUtils.Database.XunitTest/DatabaseLibTests/TestDbContext.cs
@@ -1,29 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NetCoreUtils.Database.MultiTenancy;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DatabaseLibTests
 {
-    public class TaskItem
+    public class TaskItem : TenantEntity
     {
         public int Id { get; set; }
         public string Descriptiont { get; set; }
     }
 
-    public class Project
+    public class Project : TenantEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public virtual List<TaskItem> TaskItems { get; set; } = new List<TaskItem>();
     }
 
-    public class TestDbContext : DbContext
+    //public class TestDbContext : DbContext
+    public class TestDbContext : MultiTenantContext
     {
-        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        //public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        public TestDbContext([NotNull] DbContextOptions options, ITenantProvider tenantProvider) : base(options, tenantProvider)
         {
             Database.EnsureCreated();
         }
 
         public DbSet<Project> Projects { get; set; }
         public DbSet<TaskItem> TaskItems { get; set; }
+    }
+
+    public class TenantProvider : ITenantProvider
+    {
+        public string GetTenantId()
+        {
+            return "tenant1";
+        }
     }
 }
