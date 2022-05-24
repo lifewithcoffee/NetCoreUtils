cdd7635d91c6d1b0914b576f0a23515663a8209c
li-rongcheng <li.rongcheng@gmail.com>
2022-05-25 03:56:42 +1000

    reorganize files

---
+++ McnLib/Parsers/FileLineParser.cs
@@ -0,0 +1,27 @@
+﻿using McnLib.FileProcessing;
+using McnLib.Structures;
+
+namespace McnLib.Parsers
+{
+    public class FileLineParser
+    {
+        ILineParser[] parsers = { new SectionParser(), new BlockParser() };
+        public void ParseLines(List<FileLine> lines)
+        {
+            foreach (var line in lines)
+            {
+                foreach (var parser in parsers)
+                {
+                    if (parser.ProcessLine(line))
+                        continue;
+                }
+            }
+        }
+
+        public List<Block> GetBlocks()
+        {
+            var blockParser = parsers.Where(parser => parser is BlockParser).First();
+            return ((BlockParser)blockParser).Blocks;
+        }
+    }
+}
---
+++ McnLib/Parsers/TextFileParser.cs
@@ -0,0 +1,21 @@
+﻿using McnLib.Blocks;
+using McnLib.FileProcessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McnLib.Parsers
+{
+    public class TextFileParser
+    {
+        TextFileReader fileReader = new TextFileReader();
+        FileLineParser lineParser = new FileLineParser();
+        public void ParseFile(string filePath)
+        {
+            var lines = fileReader.ReadFile(filePath);
+            lineParser.ParseLines(lines);       // TODO: (working) need to input an AST object
+        }
+    }
+}
