73cabe5bf0ab4df640636eda57316d0718f94487
li-rongcheng <li.rongcheng@gmail.com>
2022-05-26 02:43:11 +1000

    Update NoteFile.cs

--- McnLib/States/NoteFile.cs
+++ McnLib/States/NoteFile.cs
@@ -5,7 +5,12 @@ namespace McnLib.States
     public class NoteFile
     {
         public string FullPath { get; set; } = "";
+
+        /// <summary>
+        /// All blocks of this file.
+        /// </summary>
         public List<Block> Blocks { get; set; } = new List<Block>();
+
         public List<Section> Sections { get; set; } = new List<Section>();
     }
 }
