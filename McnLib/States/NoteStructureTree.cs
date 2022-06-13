b9ce125279909161d69e551f989668cb3ef67be8
li-rongcheng <li.rongcheng@gmail.com>
2022-06-14 02:22:36 +1000

    Implement NoteStructureTree.FindNotes()

--- McnLib/States/NoteStructureTree.cs
+++ McnLib/States/NoteStructureTree.cs
@@ -2,13 +2,34 @@
 
 namespace McnLib.States
 {
+    public class FindNotesResult
+    {
+        public string FilePath { get; set; } = "";
+        public List<NoteLine> LinesFound { get; set; } = new List<NoteLine>();
+    }
+
     public class NoteStructureTree
     {
-        public List<NoteFile>? NoteFiles { get; set; }
+        public List<NoteFile> NoteFiles { get; set; } = new List<NoteFile>();
 
-        public List<Note> GetAllNotes()
+        public List<Note> GetAllNotes()     // TODO: useless, remove this method
         {
             return NoteFiles!.SelectMany(f => f.Notes).ToList();
+        }
+
+        public List<FindNotesResult> FindNotes(string[] keywords)
+        {
+            List<FindNotesResult> results = new List<FindNotesResult>();
+            foreach(var file in NoteFiles)
+            {
+                FindNotesResult result = new FindNotesResult { FilePath = file.FullPath };
+                file.Notes.ForEach(n => {
+                    var lines = n.FindLinesAND(keywords);
+                    if (lines != null)
+                        result.LinesFound.AddRange(lines);
+                });
+            }
+            return results;
         }
     }
 }
