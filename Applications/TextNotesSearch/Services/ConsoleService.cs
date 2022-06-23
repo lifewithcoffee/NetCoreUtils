c5f41ab1076f6435ef6426a8efbf150826935ed6
li-rongcheng <li.rongcheng@gmail.com>
2022-06-24 02:48:01 +1000

    tns: refactor to extract IConsoleService

---
+++ Applications/TextNotesSearch/Services/ConsoleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextNotesSearch.Services
+{
+    public interface IConsoleService
+    {
+        void WriteLine(string msg, ConsoleColor color);
+    }
+    public class ConsoleService : IConsoleService
+    {
+        public void WriteLine(string msg, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
