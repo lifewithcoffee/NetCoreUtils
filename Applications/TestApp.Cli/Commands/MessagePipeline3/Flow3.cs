73d0bfac181c6805fc1a50e755c2b833331282c9
Ron <li.rongcheng@gmail.com>
2024-07-26 02:59:16 +1000

    Add console output

--- Applications/TestApp.Cli/Commands/MessagePipeline3/Flow3.cs
+++ Applications/TestApp.Cli/Commands/MessagePipeline3/Flow3.cs
@@ -1,10 +1,23 @@
-﻿namespace TestApp.Cli.Commands.MessagePipeline3;
+﻿using System;
+
+namespace TestApp.Cli.Commands.MessagePipeline3;
 
 public class Flow3
 {
     public Flow2 ParentMsg { get; set; }
 
-    public Flow3 DoAction3_1() { return this; }
-    public Flow3 DoAction3_2() { return this; }
-    public Flow3 DoAction3_3() { return this; }
+    public Flow3 DoAction3_1() {
+        Console.WriteLine("DoAction3_1");
+        return this; 
+    }
+    public Flow3 DoAction3_2()
+    { 
+        Console.WriteLine("DoAction3_2");
+        return this;
+    }
+    public Flow3 DoAction3_3()
+    {
+        Console.WriteLine("DoAction3_3");
+        return this;
+    }
 }
